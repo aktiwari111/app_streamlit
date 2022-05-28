61744a473ddea2829adea5f0de3a997cb2df4241
aktiwari680 <studenttraining111@gmail.com>
2022-05-28 13:10:34 +0530

    first commit22

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ st.write("""
 # Welcome To GLA University...
 """)
 
-"""
+st.write("""
 ## Superstar Amir Khan
-"""
+""")
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
 
 
 eq = st.text_input('Calculator')
-st.write('The current number is ',eval(eq))
+print(type(eq),eq)
+st.write('The current number is ', eval(eq))
