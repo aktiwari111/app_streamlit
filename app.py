7c5eace4a3d0e63fecdd1fd3cfbdc39459cd66f6
aktiwari680 <studenttraining111@gmail.com>
2022-05-28 13:13:29 +0530

    first commit22

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ st.write('The current movie title is', title)
 
 
 eq = st.text_input('Calculator')
-print(type(eq),eq)
-st.write('The current number is ', eval(eq))
+if eq:
+
+    st.write('The current number is ', eval(eq))
+else:
+    eq = ""
